26abf9e331fbab256b5e7940a7eca7df2865f1d8
Jacqueline Askins <amath@sas.upenn.edu>
2017-12-12 16:45:03 +0000

    More Migrations Errors

--- db/migrate/20171211201604_remove_buyer_from_transactions.rb
+++
@@ -1,5 +0,0 @@
-class RemoveBuyerFromTransactions < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :transactions, :buyer, :integer
-  end
-end
--- db/migrate/20171212042139_remove_photo_from_item.rb
+++
@@ -1,5 +0,0 @@
-class RemovePhotoFromItem < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :items, :image, :photo
-  end
-end
